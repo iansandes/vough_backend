66a6846efa7d2a9a70a2db562cc4d05a57e494d0
Ian Sandes <iansandes15@gmail.com>
2021-02-18 20:20:54 -0300

    Remocao de import nao utilizado em views.py

--- api/views.py
+++ api/views.py
@@ -8,7 +8,6 @@ from .serializers import OrganizationSerializer
 from .integrations.github import GithubApi
 
 from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Pastebin API')
 

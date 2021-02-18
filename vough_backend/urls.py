c2cd5039d04dbaa18455da6d447b0fb003d9862a
Ian Sandes <iansandes15@gmail.com>
2021-02-18 20:04:24 -0300

    Integracao da api com o swagger

--- vough_backend/urls.py
+++ vough_backend/urls.py
@@ -1,4 +1,5 @@
 """vough_backend URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -14,8 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Vough Backend API",
+        default_version='v1',
+        description="Api desenvolvida para a Vough, com o intuito de facilitar a buscar por organizações no github.",
+        contact=openapi.Contact(email="iansandes15@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.routes"), name="api"),
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+
+    path('api/api.json/', schema_view.without_ui(cache_timeout=0),
+         name='schema-swagger-ui'),
 ]

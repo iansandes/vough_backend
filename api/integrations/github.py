e4b7c40858773e9f9fc3cd8a3004523abf43746b
Ian Sandes <iansandes15@gmail.com>
2021-02-18 20:01:52 -0300

    Criacao das funcionalidades de integracao com o github

--- api/integrations/github.py
+++ api/integrations/github.py
@@ -1,19 +1,31 @@
 import os
 import requests
+import json
 
 
 class GithubApi:
-    API_URL = os.environ.get("GITHUB_API_URL", "https://api.github.com")
-    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    def __init__(self):
+        self.GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
     def get_organization(self, login: str):
         """Busca uma organização no Github
+
         :login: login da organização no Github
         """
-        return {}
+        headers = {'Authorization': self.GITHUB_TOKEN}
+        response = requests.get(
+            f'https://api.github.com/orgs/{login}', headers=headers)
+        return response.json(), response.status_code
 
     def get_organization_public_members(self, login: str) -> int:
         """Retorna todos os membros públicos de uma organização
+
         :login: login da organização no Github
         """
-        return 0
+        headers = {'Authorization': self.GITHUB_TOKEN}
+        response = requests.get(
+            f'https://api.github.com/orgs/{login}/members', headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response
